a234ad62f3afa957e9a98850eeb547204d4c6f02
studentszO <169681673+studentszO@users.noreply.github.com>
2024-10-26 18:18:27 +0200

    Add test for sunk method & fix an expect value for boat verticality

--- src/modules/board.test.js
+++ src/modules/board.test.js
@@ -1,5 +1,9 @@
 import Board from "./board";
 import Ship from "./ships";
+import Player from "./players";
+
+const x = new Player("szo");
+console.log(x.board);
 
 describe("Place ships tests", () => {
   const board = new Board();
