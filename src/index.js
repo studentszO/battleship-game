72e699a3aaa56a64cb36bccaf5dd94bed1f156ef
studentszO <169681673+studentszO@users.noreply.github.com>
2024-10-28 10:00:56 +0100

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
+/* global document */
 import "./style.css";
 import Board from "./modules/board";
 import Player from "./modules/players";
 import makeLineWithColumns from "./modules/dom";
 
-const container = document.querySelector(".player-one-board");
-for (let i = 0; i < 10; i++) container.appendChild(makeLineWithColumns());
+// player must be "one" or "two"
+// one = player ~ two = computer
+function makeBoard(board, player) {
+  const container = document.querySelector(`.player-${player}-board`);
+  Object.keys(board.board).forEach((key) => {
+    container.appendChild(makeLineWithColumns(key));
+  });
+}
+
+const playerBoard = new Board();
+const computerBoard = new Board();
+
+makeBoard(playerBoard, "one");
