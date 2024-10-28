89daed6ac66038c5536c30390ffc4d86f484421a
studentszO <169681673+studentszO@users.noreply.github.com>
2024-10-28 09:07:30 +0100

    Add a board to the UI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./style.css";
+import Board from "./modules/board";
+import Player from "./modules/players";
+import makeLineWithColumns from "./modules/dom";
+
+const container = document.querySelector(".player-one-board");
+for (let i = 0; i < 10; i++) container.appendChild(makeLineWithColumns());
