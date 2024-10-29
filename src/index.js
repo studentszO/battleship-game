004300b26a831cc902b9587f1976f92983a31867
studentszO <169681673+studentszO@users.noreply.github.com>
2024-10-29 18:44:51 +0100

    Move functions to DOM module

--- src/index.js
+++ src/index.js
@@ -1,109 +1,19 @@
-/* global document */
 import "./style.css";
-import Ship from "./modules/ships";
 import Player from "./modules/players";
-import makeCellsLine from "./modules/dom";
+import { makeBoard, eventListener, addShip } from "./modules/dom";
 
-// player must be "one" or "two"
-// one = player ~ two = computer
-function makeBoard(board, player) {
-  const container = document.querySelector(`.player-${player}-board`);
-  Object.keys(board.cells).forEach((key) => {
-    const lineOfCells = makeCellsLine(key);
-    lineOfCells.forEach((node) => container.append(node));
-  });
-}
-
-let boatNumber = 0;
-
-function getPlayerBoardNode(player) {
-  const whichPlayer = player.name === "Computer" ? "two" : "one";
-  return document.querySelector(`.player-${whichPlayer}-board`);
-}
-
-function handleCellSplit(cell) {
-  const [x, y] = cell.match(/[A-Z]+|[0-9]+/g);
-  return [x, y];
-}
-
-function addShip(player, shipLength, cell, orientation) {
-  // Separate cell arg into it's letter and it's number
-  const coordinates = handleCellSplit(cell);
-  const newShip = new Ship(shipLength);
-
-  player.board.placeShip(newShip, coordinates, orientation);
-
-  const boatsArray = [];
-
-  Object.keys(player.board.cells).forEach((key) =>
-    player.board.cells[key].forEach((cellID, index) => {
-      const toPush = [key + (index + 1), boatNumber];
-      if (typeof cellID !== "number" && cellID === newShip)
-        boatsArray.push(toPush);
-    }),
-  );
-
-  const boatsCells = boatsArray.map((coordinates) => [
-    getPlayerBoardNode(player).querySelector(
-      `div[data-cell='${[...coordinates][0]}']`,
-    ),
-    coordinates[1],
-  ]);
-
-  boatsCells.forEach((node, index) => {
-    node[0].classList.add(`boat${node[1]}`);
-    if (index === 0) node[0].classList.add(`boat-start-${orientation}`);
-    if (index === boatsCells.length - 1)
-      node[0].classList.add(`boat-end-${orientation}`);
-    if (index > 0 && index < boatsCells.length - 1)
-      node[0].classList.add(`boat-mid-${orientation}`);
-  });
-}
-
-function updateBoardWhenAttacking(boardNode, cell) {
-  const node = boardNode.querySelector(`[data-cell=${cell}]`);
-  const hitOrMiss = node.className.match(/boat/) ? "hit" : "miss";
-  node.classList.add(hitOrMiss);
-}
-
-function attackCell(player, cell) {
-  const coordinates = handleCellSplit(cell);
-  const gameBoardCell = player.board.cells[coordinates[0]][coordinates[1] - 1];
-  if (typeof gameBoardCell !== "number") {
-    gameBoardCell.hit();
-  }
-  updateBoardWhenAttacking(getPlayerBoardNode(player), cell);
-}
-
-function eventListener(players) {
-  const gameContainer = document.querySelector(".game-container");
-  gameContainer.addEventListener("click", (event) => {
-    if (event.target.classList.contains("clicked")) return;
-
-    const cell = event.target.getAttribute("data-cell");
-    event.target.classList.add("clicked");
-    const parentNode = event.target.parentElement.className;
-    const playerToAttack = parentNode.match(/two/) ? players[1] : players[0];
-
-    if (cell) attackCell(playerToAttack, cell);
-  });
-}
-
+// Create players
 const player = new Player("chryszO");
 const computer = new Player("Computer");
-eventListener([player, computer]);
 
+// Make the boards for each player
 makeBoard(player.board, "one");
 makeBoard(player.board, "two");
 
-// FOR TESTING PURPOSES ONLY!
-// PLACE SHIP ON C3 !
-// player.board.placeShip(new Ship(3), ["C", 3], "h");
+// Start listening to the DOM events
+eventListener([player, computer]);
 
-// HIT THE SHIP ON C3!
-// player.board.cells["C"][2].hit();
-console.log(player.board);
-console.log(computer.board);
+// Add ships for each player
 addShip(player, 3, "C2", "h");
 addShip(player, 5, "A3", "h");
 addShip(player, 2, "J1", "h");
@@ -115,3 +25,11 @@ addShip(computer, 5, "A3", "h");
 addShip(computer, 2, "J1", "h");
 addShip(computer, 4, "H6", "h");
 addShip(computer, 3, "F2", "h");
+
+// FOR TESTING PURPOSES ONLY!
+
+// PLACE SHIP ON C3 !
+// player.board.placeShip(new Ship(3), ["C", 3], "h");
+
+// HIT THE SHIP ON C3!
+// player.board.cells["C"][2].hit();
--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,5 +1,11 @@
 /* eslint-disable no-undef */
-const makeCellsLine = (cell) => {
+/* global document */
+
+import Ship from "./ships";
+
+let boatNumber = 0;
+
+function makeCellsLine(cell) {
   const lineDiv = [];
 
   const makeCell = (cellID) => {
@@ -11,6 +17,87 @@ const makeCellsLine = (cell) => {
   for (let i = 0; i < 10; i++) lineDiv.push(makeCell(`${cell}${i + 1}`));
 
   return lineDiv;
-};
+}
 
-export default makeCellsLine;
+// player must be "one" or "two"
+// one = player ~ two = computer
+export function makeBoard(board, player) {
+  const container = document.querySelector(`.player-${player}-board`);
+  Object.keys(board.cells).forEach((key) => {
+    const lineOfCells = makeCellsLine(key);
+    lineOfCells.forEach((node) => container.append(node));
+  });
+}
+
+export function getPlayerBoardNode(player) {
+  const whichPlayer = player.name === "Computer" ? "two" : "one";
+  return document.querySelector(`.player-${whichPlayer}-board`);
+}
+
+function handleCellSplit(cell) {
+  const [x, y] = cell.match(/[A-Z]+|[0-9]+/g);
+  return [x, y];
+}
+
+export function addShip(player, shipLength, cell, orientation) {
+  // Separate cell arg into it's letter and it's number
+  const coordinates = handleCellSplit(cell);
+  const newShip = new Ship(shipLength);
+
+  player.board.placeShip(newShip, coordinates, orientation);
+
+  const boatsArray = [];
+
+  Object.keys(player.board.cells).forEach((key) =>
+    player.board.cells[key].forEach((cellID, index) => {
+      const toPush = [key + (index + 1), boatNumber];
+      if (typeof cellID !== "number" && cellID === newShip)
+        boatsArray.push(toPush);
+    }),
+  );
+
+  const boatsCells = boatsArray.map((coordinates) => [
+    getPlayerBoardNode(player).querySelector(
+      `div[data-cell='${[...coordinates][0]}']`,
+    ),
+    coordinates[1],
+  ]);
+
+  boatsCells.forEach((node, index) => {
+    node[0].classList.add(`boat${node[1]}`);
+    if (index === 0) node[0].classList.add(`boat-start-${orientation}`);
+    if (index === boatsCells.length - 1)
+      node[0].classList.add(`boat-end-${orientation}`);
+    if (index > 0 && index < boatsCells.length - 1)
+      node[0].classList.add(`boat-mid-${orientation}`);
+  });
+}
+
+function updateBoardWhenAttacking(boardNode, cell) {
+  const node = boardNode.querySelector(`[data-cell=${cell}]`);
+  const hitOrMiss = node.className.match(/boat/) ? "hit" : "miss";
+  node.classList.add(hitOrMiss);
+}
+
+function attackCell(player, cell) {
+  const coordinates = handleCellSplit(cell);
+  const gameBoardCell = player.board.cells[coordinates[0]][coordinates[1] - 1];
+  if (typeof gameBoardCell !== "number") {
+    gameBoardCell.hit();
+  }
+  updateBoardWhenAttacking(getPlayerBoardNode(player), cell);
+}
+
+export function eventListener(players) {
+  const gameContainer = document.querySelector(".game-container");
+  gameContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("clicked")) return;
+
+    const cell = event.target.getAttribute("data-cell");
+    event.target.classList.add("clicked");
+    const parentNode = event.target.parentElement.className;
+    const playerToAttack = parentNode.match(/two/) ? players[1] : players[0];
+
+    if (cell) attackCell(playerToAttack, cell);
+  });
+}
